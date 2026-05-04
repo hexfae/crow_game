19c059ab75ab9e16bf317d7f0299e707a69189bd
hexfae <hexfae@proton.me>
2026-05-04 03:15:29 +0200

    feat: translate camera to follow the leader crow

--- src/camera.rs
+++ src/camera.rs
@@ -1,16 +1,33 @@
 use bevy::prelude::*;
 
+use crate::crow::LeaderCrow;
+
+const OFFSET: Vec3 = Vec3::new(10., 10., 15.);
+const DECAY_RATE: f32 = 5.0;
+
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup)
+            .add_systems(Update, watch_leader);
     }
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(10., 10., 15.).looking_at(Vec3::Y * 5., Vec3::Y),
+        Transform::from_translation(OFFSET).looking_at(Vec3::Y, Vec3::Y),
     ));
+}
+
+fn watch_leader(
+    mut camera: Single<&mut Transform, With<Camera3d>>,
+    leader: Single<&Transform, (With<LeaderCrow>, Without<Camera3d>)>,
+    time: Res<Time>,
+) {
+    let offset = leader.translation + Vec3::new(10., 10., 15.);
+    camera
+        .translation
+        .smooth_nudge(&offset, DECAY_RATE, time.delta_secs());
 }
